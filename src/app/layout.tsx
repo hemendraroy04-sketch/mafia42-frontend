c7257e99aecd9b1ba5bd88fdf00bd41705f008d2
Hemendra <hemendraroy04@gmail.com>
2026-09-20 16:19:03 +0530

    added analytics and removed backendwakeup as it has been replaced by renderping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BackendWakeUp from "@/components/BackendWakeUp";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,8 +57,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <BackendWakeUp />
         {children}
+        <Analytics />
       </body>
     </html>
   );
